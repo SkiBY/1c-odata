a4bac85c90c4d1134b9248bd2fccc864205bd5ce
Sergei Ruzki <sergei.ruzki@gmail.com>
2022-06-05 12:39:24 +0300

    try to do via parse

--- odata1cw/utils.py
+++ odata1cw/utils.py
@@ -1,8 +1,12 @@
+from urllib.parse import urlencode
+
 def make_url_part(name, value, value_type):
     if value == None:
         return ''
     if type(value) != value_type:
         raise ValueError('{}={} must be {}'.format(
             name, value, value_type))
-    result = "&${}={}".format(name, value)
+
+    result = '&'+urlencode({'$'+name: value})
+    # result = urlencode({}"&${}={}".format(name, value))
     return result
